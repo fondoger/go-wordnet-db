9b9886173e0aec781dba214fbfbd3da3f516635e
fondoger <fondoger@outlook.com>
2024-10-31 14:05:12 +0800

    Add print files

--- wordnet-db.go
+++ wordnet-db.go
@@ -1,6 +1,23 @@
 package gowordnetdb
 
-import "embed"
+import (
+	"embed"
+	"io/fs"
+)
 
 //go:embed wordnet-2023/*
-var WordnetDataFS embed.FS
+var rawDir embed.FS
+
+var WordnetDataFS, _ = fs.Sub(rawDir, "wordnet-2023")
+
+// print all files in the wordnet-2023 directory
+func PrintAllFiles() {
+	println("All files in the wordnet-2023 directory:")
+	_ = fs.WalkDir(WordnetDataFS, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		println(path)
+		return nil
+	})
+}
